53143549673851251fe9dcdba832d0a6c05b1913
buddhar2 <buddhar2@uwm.edu>
2022-02-26 22:03:57 -0600

    Committing latest changes

---
+++ app/http/controllers/authController.js
@@ -0,0 +1,15 @@
+const User = require('../../models/user')
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+function authController() {
+    const _getRedirectUrl = (req) => {
+        return req.user.role === 'admin' ? '/admin/orders' : '/customer/orders'
+    }
+    
+    return {
+        login(req, res) {
+            res.render('auth/login')
+        }
+    }
+}
+module.exports = authController
