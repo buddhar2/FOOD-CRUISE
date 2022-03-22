34703809cc121e9f4b4d98a658ca7534c1ba891a
SharanKrishna <sharankrishna@gmail.com>
2022-03-21 22:37:27 -0500

    updated routes and css file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,29 @@
-console.log('Hello from app js')
+import axios from 'axios'
+import Noty from 'noty'
+
+let addToCart = document.querySelectorAll('.add-to-cart')
+let cartCounter = document.querySelector('#cartCounter')
+
+function updateCart(item) {
+    axios.post('/update-cart', item).then(res => {
+        cartCounter.innerText = res.data.totalQty
+        new Noty({
+            type: 'success',
+            timeout: 1000,
+            text: 'Item added to cart',
+        }).show();
+    }).catch(err => {
+        new Noty({
+            type: 'error',
+            timeout: 1000,
+            text: 'Something went wrong',
+        }).show();
+    })
+}
+
+addToCart.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        let item = JSON.parse(btn.dataset.item)
+        updateCart(item)
+    })
+})
