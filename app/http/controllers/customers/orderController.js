85d0a6f01681260b81e3a2c8ac28d91860f49bbf
buddhar2 <buddhar2@uwm.edu>
2022-03-30 21:03:44 -0500

    orders page and controller

---
+++ app/http/controllers/customers/orderController.js
@@ -0,0 +1,35 @@
+const { response } = require('express')
+const Order = require('../../../models/order')
+
+function orderController () {
+    return {
+        store(req, res) {
+
+            const { phone, address } = req.body
+            if(!phone || !address){
+                req.flash('error', 'All fields are required')
+                return res.redirect('/cart')
+            }
+
+            const order = new Order({
+                customerId: req.user._id,
+                items: req.session.cart.items,
+                phone: phone,
+                address: address
+            })
+            order.save().then(result => {
+                req.flash('success', 'Order placed successfully')
+                return res.redirect('/')
+            }).catch(err => {
+                req.flash('error','Something went wrong')
+                return res.redirect('/cart')
+            })
+        },
+        async index(req, res) {
+            const orders = await Order.find({ customerId: req.user._id })
+            res.render('customers/orders', { orders: orders })
+        }
+    }
+}
+
+module.exports = orderController
