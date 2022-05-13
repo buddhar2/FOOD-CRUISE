cd30ffb73f686e5c217f3e6f44772cf3ab776306
Manjarikundam <manjarikundam@gmail.com>
2022-05-13 11:28:40 -0500

    Committing latest changes

--- routes/web.js
+++ routes/web.js
@@ -1,3 +1,4 @@
+
 const homeController= require('../app/http/controllers/homeController')
 const authController = require('../app/http/controllers/authController')
 const cartController = require('../app/http/controllers/customers/cartController')
